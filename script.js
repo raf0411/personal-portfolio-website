eb895936e6e3b180bc4247ae16753ba3bdcbb7de
Raffi Darmawan <raffidarmawan0411@gmail.com>
2024-11-12 14:06:36 +0700

    fixed lightmode

--- script.js
+++ script.js
@@ -1,18 +1,27 @@
 const darkLightModeBtn = document.getElementById("dark-light-mode-btn");
-const body = document.querySelector("body");
 let lightmode = localStorage.getItem('lightmode');
+
+checkLightMode();
+
+function enableLightMode() {
+  localStorage.setItem("lightmode", "active");
+  document.body.classList.add("lightmode");
+}
+
+function enableDarkMode() {
+  localStorage.setItem("lightmode", null);
+  document.body.classList.remove("lightmode");
+}
+
+function checkLightMode() {
+  if (lightmode === "active") {
+    enableLightMode();
+  } else {
+    enableDarkMode();
+  }
+}
 
 darkLightModeBtn.addEventListener("click", () => {
   lightmode = localStorage.getItem('lightmode');
   lightmode !== "active" ? enableLightMode() : enableDarkMode();
 });
-
-function enableLightMode() {
-  localStorage.setItem("lightmode", "active");
-  body.classList.add("lightmode");
-}
-
-function enableDarkMode() {
-  localStorage.setItem("lightmode", null);
-  body.classList.remove("lightmode");
-}
