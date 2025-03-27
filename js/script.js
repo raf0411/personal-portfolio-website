0a7d4f31f10c8caff3a6a0357bb411e931908dfb
Raffi <raffidarmawan0411@gmail.com>
2025-03-27 21:45:50 +0700

    added sending email functionality

--- js/script.js
+++ js/script.js
@@ -4,9 +4,19 @@ const hiddenMenu = document.getElementById("hidden-menu");
 const closeHiddenMenuBtn = document.getElementById("close-hidden-menu-btn");
 const menuBtn = document.getElementById("menu-btn");
 
+const nameInput = document.getElementById("name");
+const emailInput = document.getElementById("email");
+const msgInput = document.getElementById("message");
+const submitBtn = document.getElementById("submit-btn");
+
+const nameErrorLbl = document.getElementById("name-error-lbl");
+const emailErrorLbl = document.getElementById("email-error-lbl");
+const msgErrorLbl = document.getElementById("msg-error-lbl");
+
 let lightmode = localStorage.getItem('lightmode');
 
 updateYear();
+refreshContactForm();
 checkLightMode();
 
 function updateYear() {
@@ -40,6 +50,36 @@ function displayHiddenMenu() {
 function removeHiddenMenu() {
   hiddenMenu.classList.remove("active");
 }
+
+function validateContactForm() {
+  if (nameInput.value === "" && emailInput.value === "" && msgInput.value === "") {
+    nameErrorLbl.value = "Name can't be empty!"
+    emailErrorLbl.value = "Email can't be empty!"
+    msgErrorLbl.value = "Message can't be empty!"
+    
+    nameErrorLbl.innerText = nameErrorLbl.value
+    emailErrorLbl.innerText = emailErrorLbl.value
+    msgErrorLbl.innerText = msgErrorLbl.value
+
+    nameErrorLbl.classList.remove("error-hidden")
+    emailErrorLbl.classList.remove("error-hidden")
+    msgErrorLbl.classList.remove("error-hidden")
+  } else {
+    nameErrorLbl.classList.add("error-hidden")
+    emailErrorLbl.classList.add("error-hidden")
+    msgErrorLbl.classList.add("error-hidden")
+  }
+}
+
+function refreshContactForm() {
+  nameInput.value = ""
+  emailInput.value = ""
+  msgInput.value = ""
+}
+
+submitBtn.addEventListener("click", () => {
+  validateContactForm()
+})
 
 closeHiddenMenuBtn.addEventListener("click", () => {
   removeHiddenMenu();
